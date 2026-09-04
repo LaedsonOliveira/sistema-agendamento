9612fdca435ce1aeb1e70c37e35bf7c4c727ebc9
laedson <laedson2535@gmail.com>
2026-09-04 20:14:34 -0300

    schema ORM v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Calendar from "./components/calendario/page";
 import FormAgendamento from "./components/form-agendamento/page";
 
 export default function Home() {
@@ -6,14 +5,6 @@ export default function Home() {
     <main className="min-h-screen bg-slate-100 p-4 md:p-8 flex flex-col items-center justify-center">
       {/* Container compacto centralizado */}
       <section className="w-full max-w-md flex flex-col gap-4">
-        <Calendar
-          events={[
-            { title: "viajar", date: "2026-08-24" },
-            { title: "viajar1", date: "2026-08-24" },
-            { title: "viajar2", date: "2026-08-24" },
-            { title: "viajar3", date: "2026-08-24" },
-          ]}
-        />
 
         <FormAgendamento />
       </section>
